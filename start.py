b2429a11d48267b6d67d1e5ad58298b49f96f2b8
Andrew <andrewmccollister1995@gmail.com>
2017-05-17 14:10:27 -0500

    Making commands dynamicallycalled
    May have peeked at SexualRhinocerous's command calls

--- start.py
+++ start.py
@@ -8,11 +8,35 @@ from discord.ext.commands import Bot
 
 # figure out how implementing this will help
 class ChatManager(Bot):
-    pass
+
+    async def incoming_message(self, message : discord.Message):
+        if message.content.startswith(self.command_prefix):
+            await getattr(self, "cmd_" + message.content[1:])(message)
+
+    async def cmd_test(self, message):
+        await self.send_message(message.channel, "If you see this, Andrew did something right.")
+
+    async def cmd_choose(self, message): # fix this
+        pass  # await self.say("The best is " + random.choice(choice))
+
+    async def cmd_hello(self, message):
+        await self.send_message(message.channel, "Hello I am {0}!".format(self.user.name))
+
+    async def cmd_me(self, message):
+        await self.send_message(message.channel, "You are {0.author.name} in {0.channel.name}".format(message))
+
+    async def cmd_joined(self, message):
+        await self.send_message(message.channel, "{0.name} joined on {0.joined_at}".format(message.author))
+
+    async def cmd_add(self, *nums: float): # fix this
+        pass
+        """sumNum = 0
+        for i in nums:
+            sumNum += i
+        await self.say("The sum of all these numbers is {0}".format(sumNum))"""
+
 
 feather_bot = ChatManager(command_prefix="!")  # command_prefix="!")
-
-quotes = open("quotes.txt", "w")
 
 @feather_bot.event
 async def on_ready():
@@ -25,30 +49,9 @@ async def on_ready():
 @feather_bot.event
 async def on_message(message : discord.Message):
     if message.author.id != feather_bot.user.id:
-        await feather_bot.send_message(message.channel, "I saw a message")  # feather_bot.say("I saw a message!")
+        await feather_bot.incoming_message(message)
+        # await feather_bot.send_message(message.channel, "I saw a message")  # feather_bot.say("I saw a message!")
 
 # calculate command "1+1" "8%2"
-@feather_bot.command()
-async def hello():
-    await feather_bot.say("Hello I am {0}!".format(feather_bot.user.name))
-
-@feather_bot.command(pass_context=True)
-async def me(ctx):
-    await feather_bot.say("You are {0.author.name} in {0.channel.name}".format(ctx.message))
-
-@feather_bot.command()
-async def joined(member : discord.Member):
-    await feather_bot.say("{0.name} joined on {0.joined_at}".format(member))
-
-@feather_bot.command()
-async def add(*nums : float):
-    sumNum = 0
-    for i in nums:
-        sumNum += i
-    await feather_bot.say("The sum of all these numbers is {0}".format(sumNum))
-
-@feather_bot.command()
-async def choose(*choice : str):
-    await feather_bot.say("The best is " + random.choice(choice))
 
 feather_bot.run(constants.BOT_TOKEN)
