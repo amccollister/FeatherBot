ec871ec4156fd5053ac2d60f6faa24ac61620ba5
Andrew <andrewmccollister1995@gmail.com>
2017-05-20 02:06:40 -0500

    Proper plugin management
    Due to HTML401 errors (Multiple bot instances), plugins now return the
    STR to be printed rather than trying to send its own message to a
    non-existant bot instance.

--- chatmanager/currency.py
+++ chatmanager/currency.py
@@ -1,8 +1,5 @@
 from chatmanager import bot
 
 class Plugin(bot.ChatManager):
-    async def cmd_testcur(self, message, *_):
-        await self.send_message(message.channel, "This is a test from the Currency Plugin")
-
-    def test(self):
-        print("cur test")
+    def cmd_testcur(self, *_):
+        return "This is a test of the currency function"
--- chatmanager/fun.py
+++ chatmanager/fun.py
@@ -1,8 +1,6 @@
 from chatmanager import bot
 
 class Plugin(bot.ChatManager):
-    async def cmd_testfun(self, message, *_):
-        await self.send_message(message.channel, "This is a test from the Fun Plugin")
+     def cmd_testfun(self, *_):
+        return "This is a test from the Fun Plugin"
 
-    def test(self):
-        print("fun test")
