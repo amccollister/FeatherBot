6a26ea46b5f62f16cb0f91c3fa838b9039bcd6e7
Andrew <amccollister@users.noreply.github.com>
2018-04-22 21:26:49 -0500

    event testing
    playing with events and various outputs

--- chatmanager/bot.py
+++ chatmanager/bot.py
@@ -1,3 +1,4 @@
+import sys
 import constants
 import configparser
 import discord
@@ -8,6 +9,8 @@ from importlib import import_module
 
 
 class ChatBot(Bot):
+    # on_reaction_add on_member_join on_member_remove
+    # on_member_update on_member_ban on_guild_join
     def __init__(self):
         super().__init__(constants.PREFIX)
 
@@ -23,8 +26,31 @@ class ChatBot(Bot):
         await self.change_presence(activity=discord.Game("with my developer"), status="dnd")
 
     async def on_message(self, message):
+        if "meme" in message.content and message.author.id != self.user.id:
+            output = [j.lower() if i%2==0 else j.upper() for i, j in enumerate(message.content)]
+            await message.channel.send("".join(output))
         print("Message from {0.author}: {0.content}".format(message))
 
+    async def on_error(self, event, *args, **kwargs):
+        print(sys.exc_info())
+        print("ERROR: {}".format(event))
+
+    async def on_typing(self, channel, user, when):
+        print("{0} starting typing in {1} at {2}".format(user, channel, when))
+
+    async def on_message_delete(self, message):
+        await message.channel.send("I saw you delete that.")
+
+    async def on_message_edit(self, before, after):
+        user = after.author
+        if before.content != after.content:
+            await after.channel.send("{} changed his message from {} to {}".format(user, before.content, after.content))
+
+    async def on_reaction_add(self, reaction, user):
+        emoji = reaction.emoji
+        if reaction.custom_emoji:
+            emoji = ":{}:".format(str(reaction.emoji).split(":")[1])
+        await reaction.message.channel.send("{} reacted with {}".format(user, emoji))
 
 # Old class to tbe adjust later
 class ChatManager(Bot):
