8ddf774984ef90348a15a5c8566f0be5940f2921
Andrew <amccollister@users.noreply.github.com>
2018-04-24 03:49:45 -0500

    Event handling and command testing
    Adding event checks
    Added first command

--- chatmanager/bot.py
+++ chatmanager/bot.py
@@ -9,8 +9,9 @@ from importlib import import_module
 
 
 class ChatBot(Bot):
-    # on_reaction_add on_member_join on_member_remove
+    # on_member_join on_member_remove
     # on_member_update on_member_ban on_guild_join
+    # on_command on_command_error on_command_completion
     def __init__(self):
         super().__init__(constants.PREFIX)
 
@@ -25,11 +26,16 @@ class ChatBot(Bot):
         print("------------")
         await self.change_presence(activity=discord.Game("with my developer"), status="dnd")
 
+    async def on_command(self, ctx):
+        print("I think I saw a command?")
+
     async def on_message(self, message):
         if "meme" in message.content and message.author.id != self.user.id:
             output = [j.lower() if i%2==0 else j.upper() for i, j in enumerate(message.content)]
             await message.channel.send("".join(output))
         print("Message from {0.author}: {0.content}".format(message))
+
+        await self.process_commands(message)
 
     async def on_error(self, event, *args, **kwargs):
         print(sys.exc_info())
@@ -41,16 +47,18 @@ class ChatBot(Bot):
     async def on_message_delete(self, message):
         await message.channel.send("I saw you delete that.")
 
-    async def on_message_edit(self, before, after):
-        user = after.author
-        if before.content != after.content:
-            await after.channel.send("{} changed his message from {} to {}".format(user, before.content, after.content))
+    async def on_raw_message_edit(self, payload):#before, after):
+        print("Data {}".format(payload.data))
+        #user = after.author
+        #if before.content != after.content:
+        #    await after.channel.send("{} changed his message from {} to {}".format(user, before.content, after.content))
 
     async def on_reaction_add(self, reaction, user):
         emoji = reaction.emoji
         if reaction.custom_emoji:
             emoji = ":{}:".format(str(reaction.emoji).split(":")[1])
         await reaction.message.channel.send("{} reacted with {}".format(user, emoji))
+
 
 # Old class to tbe adjust later
 class ChatManager(Bot):
--- start.py
+++ start.py
@@ -9,6 +9,11 @@ from chatmanager.bot import ChatBot
 
 def main():
     bot = ChatBot()
+
+    @bot.command()
+    async def test(ctx, arg):
+        await ctx.send(arg)
+
     bot.run()
 
 
